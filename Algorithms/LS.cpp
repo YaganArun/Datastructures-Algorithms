c88a1478a7cfaec03100a546e18bdeaacf032743
Yagan Arun <yaganarun1999@gmail.com>
2021-08-04 22:07:30 +0530

    Added LinearSearch

---
+++ Algorithms/LS.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int LinearSearch(int arr[], int n, int ele)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == ele)
+            return (i + 1);
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int search_ele;
+    cin >> search_ele;
+    int res = LinearSearch(arr, sizeof(arr) / sizeof(arr[0]), search_ele);
+    if (res != -1)
+        cout << res << endl;
+    else
+        cout << "NA" << endl;
+    return 0;
+}
